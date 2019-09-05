3b3856080ddb51ab0ee0dd71b464cf9070b78692
agahkarakuzu <agahkarakuzu@gmail.com>
2019-09-05 01:07:02 -0400

    watch some videos

--- NB01/Hello.py
+++ NB01/Hello.py
@@ -1,13 +1,25 @@
 import webbrowser
 
-def HelloAdele():
-	webbrowser.open_new('https://www.youtube.com/watch?v=YQHsXMglC9A')
+def HelloAdele(*args):
+	if not args:
+		webbrowser.open_new('https://www.youtube.com/watch?v=YQHsXMglC9A')
+	else:
+		return 'https://www.youtube.com/embed/YQHsXMglC9A'
 
 
-def HelloLionelRichie():
-	webbrowser.open_new('https://www.youtube.com/watch?v=UBYnT8JY7sE')
 
-def WhatKindaWorld():
-	webbrowser.open_new('https://www.youtube.com/watch?v=CWzrABouyeE')
+def HelloLionelRichie(*args):
+	if not args:
+		webbrowser.open_new('https://www.youtube.com/watch?v=UBYnT8JY7sE')
+	else: 
+		return 'https://www.youtube.com/embed/UBYnT8JY7sE'
+
+
+def WhatKindaWorld(*args):
+	if not args:
+		webbrowser.open_new('https://www.youtube.com/watch?v=CWzrABouyeE')
+	else:
+		return 'https://www.youtube.com/embed/CWzrABouyeE'
+
 
 
